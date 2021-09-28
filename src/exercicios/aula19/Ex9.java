029f91b763b8a4efa7169f103f0b26e4a466e5d8
luan3642 <luan3642@hotmail.com>
2021-09-28 12:42:20 -0300

    Update Ex9.java
    ...

--- src/exercicios/aula19/Ex9.java
+++ src/exercicios/aula19/Ex9.java
@@ -11,34 +11,30 @@ import java.util.Random;
 
 public class Ex9 {
     public static void main(String[] args) {
+        double[] vetorA = new double[10];
+        double[] vetorB = new double[vetorA.length];
+        double[] vetorC = new double[vetorA.length];
+        
         Random rd = new Random();
         
-        
-        int[] vetorA = new int[10];
-        
-        int[] vetorB = new int[10];
-        
-        
-        int[] vetorC = new int[vetorA.length];
-        
         for (int i = 0; i < 10; i++) {
-            vetorA[i] = rd.nextInt(10);
+            vetorA[i] = rd.nextInt(20);
         }
         
         for (int i = 0; i < 10; i++) {
-            vetorB[i] = rd.nextInt(20);
+            vetorB[i] = rd.nextInt(10);
         }
         
         for (int i = 0; i < 10; i++) {
-            vetorC[i] =   vetorA[i] /  vetorB[i];
+            vetorC[i] = vetorA[i] / vetorB[i];
         }
         
+        System.out.println("Valor vetorC");
         
-        for (int i : vetorC) {
-            System.out.println(i);
+        for (double d : vetorC) {
+            System.out.println(d);
         }
-        
-        
         
     }
+   
 }
