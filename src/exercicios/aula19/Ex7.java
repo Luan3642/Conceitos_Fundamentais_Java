614efe94e5f2cf84b3d76ab4ae5cb9778b3a45f8
luan3642 <luan3642@hotmail.com>
2021-09-28 12:34:47 -0300

    Update Ex7.java
    ...

--- src/exercicios/aula19/Ex7.java
+++ src/exercicios/aula19/Ex7.java
@@ -5,37 +5,33 @@
  */
 package exercicios.aula19;
 
-
 import java.util.Random;
 
 public class Ex7 {
+
     public static void main(String[] args) {
-        
         Random rd = new Random();
-        
-        int vetorA[] = new int [10];
-        
-        int vetorB[] = new int [vetorA.length];
-        
-        int vetorC[] = new int[vetorA.length];
-        
-        for (int i = 0; i <vetorA.length; i++) {
-            vetorA[i] = rd.nextInt(10);
+
+        int[] vetorA = new int[10];
+        int[] vetorB = new int[vetorA.length];
+        int[] vetorC = new int[vetorA.length];
+
+        for (int i = 0; i < 10; i++) {
+            vetorA[i] = rd.nextInt(20);
         }
-        
-        for (int i = 0; i <vetorB.length; i++) {
+
+        for (int i = 0; i < 10; i++) {
             vetorB[i] = rd.nextInt(10);
         }
-        
-        for (int i = 0; i < vetorC.length; i++) {
+
+        for (int i = 0; i < 10; i++) {
             vetorC[i] = vetorA[i] - vetorB[i];
         }
-        
-        
-       for(int i : vetorC){
-           System.out.println(i);
-       }
-        
-        
+
+        System.out.println("Valor vetorC");
+
+        for (int i : vetorC) {
+            System.out.println(i);
+        }
     }
 }
