cd49580660c9c350fcd8be9aed10c76ff0d65064
luan3642 <luan3642@hotmail.com>
2021-09-28 12:49:42 -0300

    Update Ex10.java
    ...

--- src/exercicios/aula19/Ex10.java
+++ src/exercicios/aula19/Ex10.java
@@ -7,30 +7,26 @@ package exercicios.aula19;
 
 import java.util.Random;
 import java.util.Scanner;
+
 public class Ex10 {
     public static void main(String[] args) {
-        
-        Scanner scan = new Scanner(System.in);
-        Random rd = new Random();
-        
-        int[] vetorA = new int[20];
-        
+        int[] vetorA = new int[10];
         int[] vetorB = new int[vetorA.length];
         
+        Random rd = new Random();
+     
         for (int i = 0; i <vetorA.length; i++) {
-            vetorA[i] = rd.nextInt(20);
-            
+            vetorA[i] = rd.nextInt(50);
         }
         
         for (int i = 0; i <vetorB.length; i++) {
             vetorB[i] = vetorA[i] % 2;
         }
         
-        System.out.println("");
+        System.out.println("Valor VetorB");
         
         for (int i : vetorB) {
             System.out.println(i);
         }
-        
-     }
+    }
 }
