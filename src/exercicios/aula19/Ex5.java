c8a85297fdd9e25f022b6b76d0e12e77869f0699
luan3642 <luan3642@hotmail.com>
2021-09-28 12:23:48 -0300

    Update Ex5.java
    ....

--- src/exercicios/aula19/Ex5.java
+++ src/exercicios/aula19/Ex5.java
@@ -12,39 +12,30 @@ import java.util.Random;
  * @author luan3
  */
 public class Ex5 {
-    public static void main(String[] args) {
+   public static void vetores() {
         
         Random rd = new Random();
-        
         int[] vetorA = new int[10];
-        
         int[] vetorB = new int[vetorA.length];
         
         
-        for (int i = 0; i <vetorA.length; i++) {
+        for (int i = 0; i < 10; i++) {
             vetorA[i] = rd.nextInt(20);
         }
         
-        for (int i = 0; i < vetorB.length; i++) {
+        
+        for (int i = 0; i < 10; i++) {
             vetorB[i] = vetorA[i] * i;
         }
         
-        System.out.println("");
-        System.out.println("Valor do vetor A");
-        
-        
-        for(int i : vetorA){
+        System.out.println("Valor vetorB");
+        for (int i : vetorB) {
             System.out.println(i);
         }
         
-        System.out.println("");
-        System.out.println("Valor do vetoer B");
-        
-        for(int j : vetorB){
-            System.out.println(j);
-        }
-        
-        
-        
+    }
+
+    public static void main(String[] args) {
+        vetores();
     }
 }
