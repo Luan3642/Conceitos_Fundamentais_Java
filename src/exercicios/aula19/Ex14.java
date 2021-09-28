a0a38b2e4cc369fdd545253d41bc689ece8e8155
luan3642 <luan3642@hotmail.com>
2021-09-28 14:03:45 -0300

    Update Ex14.java
    ...

--- src/exercicios/aula19/Ex14.java
+++ src/exercicios/aula19/Ex14.java
@@ -8,30 +8,26 @@ package exercicios.aula19;
 import java.util.Random;
 
 public class Ex14 {
-    public static void main(String[] args) {
-        
-        Random rd = new Random();
+      public static void main(String[] args) {
         int[] vetorA = new int[10];
+        Random rd = new Random();
+        int media =0, soma = 0, qtdeImpar = 0;
         
-        int soma = 0;
-        float media = 0;
-        
-        for (int i = 0; i <vetorA.length; i++) {
-            vetorA[i] = rd.nextInt(10);
-            if(vetorA[i] % 2 == 1){
-                soma += vetorA[i];
-                
-                media = soma / vetorA.length;
+        for (int i = 0; i < 10; i++) {
+            vetorA[i] = rd.nextInt(20);
+            if (vetorA[i] % 2 == 1) {
+               qtdeImpar++;
+               soma += vetorA[i];
+               media = soma / qtdeImpar;
             }
-            
-        }
+        } 
         
+        System.out.println("Vetor A");
         for (int i : vetorA) {
             System.out.println(i);
         }
         
-        System.out.println("");
-        System.out.println("A média é: "+media);
-        
+        System.out.println("Media: "+ media);
+
     }
 }
