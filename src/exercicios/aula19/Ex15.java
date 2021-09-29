f6920cb62e9ba2f3fb7f44eb82ff94de45bd7ef0
luan3642 <luan3642@hotmail.com>
2021-09-29 13:54:15 -0300

    Update Ex15.java

--- src/exercicios/aula19/Ex15.java
+++ src/exercicios/aula19/Ex15.java
@@ -9,45 +9,29 @@ import java.util.Random;
 
 public class Ex15 {
 
-    public static void main(String[] args) {
-
-        Random rd = new Random();
-
-        int pares = 0;
-        int impares = 0;
- 
-
+   public static void main(String[] args) {
         int[] vetorA = new int[10];
-
-        for (int i = 0; i < vetorA.length; i++) {
+        
+        Random rd = new Random();
+        
+        int impar = 0;
+        
+        for (int i = 0; i <vetorA.length; i++) {
             vetorA[i] = rd.nextInt(20);
-
-            if (vetorA[i] % 2 != 0) {
-                impares++;
-            } 
+            if(vetorA[i] % 2 != 0)
+                impar++; 
         }
         
-        System.out.println("");
-        for (int i : vetorA) {
+        for(int i : vetorA){
             System.out.println(i);
         }
         
-        System.out.println("");
+        int par = vetorA.length - impar ;
         
-        int par = vetorA.length - impares;
+        int porcentagemPar = (par * 100) / vetorA.length;
+        int porcentagemImpar = 100 - porcentagemPar; 
         
-        // vetorA.length --- 100%
-        // par           --- x
-        // x . vetorA.legth = par * 100
-        // x == (par * 100) / vetor.length
-        
-        int porcPares = (par * 100) / vetorA.length;
-        int porcImpares = 100 - porcPares;
-        
-        
-        System.out.println("Porcentagem pares: "+porcPares +"%");
-        System.out.println("Porcentagem impares: "+porcImpares +"%");
- 
-
+        System.out.println("Porcentagem pares: "+porcentagemPar +"%");
+        System.out.println("Porcentagem impares: "+porcentagemImpar +"%");
     }
 }
