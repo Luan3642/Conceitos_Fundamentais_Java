7b48fa582d20237e11947fca9a3be8bd912a5ecf
luan3642 <luan3642@hotmail.com>
2021-09-29 22:02:15 -0300

    Update Ex16.java

--- src/exercicios/aula19/Ex16.java
+++ src/exercicios/aula19/Ex16.java
@@ -12,43 +12,35 @@ public class Ex16 {
     public static void main(String[] args) {
         int[] vetorA = new int[10];
 
-        int soma = 0;
-        int somaMaior15 =0;
-        int iguaisA15 = 0;
-        float media = 0;
-
         Random rd = new Random();
-
+        int soma = 0;
+        int igual15 = 0;
+        int somaMaior15 = 0;
+        double media = 0;
+        int aux = 0;
         for (int i = 0; i < vetorA.length; i++) {
             vetorA[i] = rd.nextInt(50);
-            
-            if(vetorA[i] < 15){
+            if (vetorA[i] < 15) {
                 soma += vetorA[i];
             }
-            
-            
-            if(vetorA[i] == 15){
-                iguaisA15++;
+            if (vetorA[i] == 15){
+                igual15++;
             }
-            
-            if(vetorA[i] > 15){
+            if (vetorA[i] > 15){
                 somaMaior15 += vetorA[i];
-                media = somaMaior15 / vetorA.length;
-                
+                aux++;
+                media = somaMaior15 / aux;
             }
-      
         }
-        System.out.println("");
         
         for (int i : vetorA) {
             System.out.println(i);
         }
-
-
         System.out.println("");
-
-        System.out.println("A soma dos números menores que 15: " + soma);
-        System.out.println("Números iguais a 15: " + iguaisA15);
-        System.out.println("Média dos números: " + media);
+        
+        
+        System.out.println("Soma dos menores que 15: "+soma);
+        System.out.println("Quantidade dos iguais a 15: "+igual15);
+        System.out.println("Media dos maiores que 15: "+media);
     }
 }
