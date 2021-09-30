fa4edd470a8cbdf6fc7f047c9d72af068951d233
luan3642 <luan3642@hotmail.com>
2021-09-30 12:18:51 -0300

    Update Ex19.java

--- src/exercicios/aula19/Ex19.java
+++ src/exercicios/aula19/Ex19.java
@@ -9,33 +9,30 @@ package exercicios.aula19;
 import java.util.Scanner;
 
 public class Ex19 {
-    public static void main(String[] args) {
-        
-        Scanner scan = new Scanner(System.in);
-        
-        
-        int[] alunos = new int[10];
+     public static void main(String[] args) {
         float[] nota1 = new float[10];
         float[] nota2 = new float[10];
-        
         float[] result = new float[10];
-        
-        for (int i = 0; i <alunos.length; i++) {
-            System.out.println("Informe a primeira nota do " + (i+1) + " aluno");
-            nota1[i] = scan.nextFloat();
-            System.out.println("Informe a segunda nota do "+ (i+1)+ " aluno");
-            nota2[i] = scan.nextFloat();
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < nota1.length; i++) {
+            System.out.println("Informe a primeira nota, do aluno " + (i + 1));
+            nota1[i] = scan.nextInt();
+
+            System.out.println("informe a segunda nota, do aluno " + (i + 1));
+            nota2[i] = scan.nextInt();
             
             result[i] = (nota1[i] + nota2[i]) / 2;
             
             if(result[i] > 7){
-                System.out.println("O "+(i+1)+ " aluno, foi aprovado");
-                System.out.println("");
-            } else{
-                System.out.println("O "+(i+1)+ " aluno., foi reprovado");
-                System.out.println("");
+                System.out.println("Aprovado, aluno " +(i+1));
+            }else{
+                System.out.println("Reprovado, aluno " +(i+1));
             }
-            
         }
+
+       
+
+        
     }
 }
