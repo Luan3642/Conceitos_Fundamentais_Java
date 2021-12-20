8e8cef4956a49a928408f5efea8b073098cf4301
luan3642 <luan3642@hotmail.com>
2021-12-20 20:03:57 -0300

    ...

--- src/exercicios/aula19/Ex31.java
+++ src/exercicios/aula19/Ex31.java
@@ -12,9 +12,33 @@ package exercicios.aula19;
 public class Ex31 {
     public static void main(String[] args) {
         
-        for (int i = 0, j = 0; i <10 && j <3; i++, j++) {
-            System.out.println(i);
-            System.out.println(j);
+        
+        int[] vetorA = {1,2,3,4,5,6,9,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+        int[] vetorB = new int[vetorA.length];
+        
+        
+        int posB = 0;
+        
+       
+        
+        for (int i = 0; i <vetorA.length; i++) {
+            if(vetorA[i] % 2 != 0){
+                vetorB[posB] = vetorA[i];
+                posB++;
+            }
         }
+        
+        
+         for (int i = 0; i < vetorA.length; i++) {
+            if(vetorA[i] % 2 == 0){
+                vetorB[posB] = vetorA[i];
+                posB++;
+            }
+        }
+        
+        for (int i = 0; i <posB; i++) {
+            System.out.print(vetorB[i] +" ");
+        }
+        
     }
 }
