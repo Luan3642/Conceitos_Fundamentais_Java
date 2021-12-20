fb3a017102dff1606f4c857cbdad494a4c12749a
luan3642 <luan3642@hotmail.com>
2021-12-20 09:47:31 -0300

    ...

--- src/exercicios/aula19/Ex29.java
+++ src/exercicios/aula19/Ex29.java
@@ -13,7 +13,23 @@ public class Ex29 {
 
     public static void main(String[] args) {
 
-    
+        int[] vetorA = {10, 2, 36, 9, 8, 7, 412, 23, 5, 9};
+        int[] vetorB = {20, 3, 6, 9, 8, 4, 7, 2, 5, 4};
 
+        int[] vetorC = new int[20];
+
+        for (int i = 0; i < vetorA.length; i++) {
+            vetorC[i] = vetorA[i];
+        }
+        
+        for (int i =10; i < vetorC.length; i++) {
+            vetorC[i] = vetorB[i - 10];
+        }
+
+        System.out.println("");
+        for (int i : vetorC) {
+            System.out.println(i);
+        }
+        
     }
 }
