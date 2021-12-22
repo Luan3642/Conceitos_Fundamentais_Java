8421f4fb36be06898eac07f9713f9164d65a7d75
luan3642 <luan3642@hotmail.com>
2021-12-22 13:49:58 -0300

    ...

--- src/exercicios/aula19/Ex35.java
+++ src/exercicios/aula19/Ex35.java
@@ -10,5 +10,17 @@ package exercicios.aula19;
  * @author Luan
  */
 public class Ex35 {
-    
+
+    public static void main(String[] args) {
+        int[] vetorA = {2, 3, 4, 5, 6, 7, 8};
+        
+        
+        for (int i = 0; i <vetorA.length; i++) {
+            for (int j = 1; j <=vetorA[i]; j++) {
+                if(vetorA[i] % j == 0){
+                    System.out.print(j + " ");
+                }
+            }
+        }
+    }
 }
