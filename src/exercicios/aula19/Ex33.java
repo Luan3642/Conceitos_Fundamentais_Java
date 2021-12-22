53061fb82ad836ad2ce630f4982821ef8a9d59df
luan3642 <luan3642@hotmail.com>
2021-12-21 23:17:01 -0300

    ...

---
+++ src/exercicios/aula19/Ex33.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios.aula19;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Luan
+ */
+public class Ex33 {
+
+    Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int[] vetorA = new int[10];
+
+        boolean primo = true;
+        for (int i = 0; i < vetorA.length; i++) {
+            System.out.println("Digite um número");
+            vetorA[i] = scan.nextInt();
+            
+            for (int j = 2; j <vetorA[i]; j++) {
+                if(vetorA[i] % j == 0){
+                    primo = false;
+                } 
+            }
+            System.out.println(primo);
+            
+            
+            
+        }
+
+    }
+}
