6251dc0cc0a51ec67b68fb4d9a0821e5622e1780
luan3642 <luan3642@hotmail.com>
2021-12-22 15:19:31 -0300

    ...

---
+++ src/exercicios/aula19/Ex36.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios.aula19;
+
+/**
+ *
+ * @author Luan
+ */
+public class Ex36 {
+    public static void main(String[] args) {
+        int[] vetorA = {0,1,2,3,4,5,6,7,8,9,10};
+        
+        for (int i = 0; i <vetorA.length; i++) {
+                System.out.println(vetorA[i] = (int)Math.pow(vetorA[i], i));
+            }
+        }
+        
+    }
+   
