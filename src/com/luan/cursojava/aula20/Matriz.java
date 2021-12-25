1bf7dc8a131425b42ed13b64036d5719029688fd
luan3642 <luan3642@hotmail.com>
2021-12-24 21:19:28 -0300

    ...

--- src/com/luan/cursojava/aula20/Matriz.java
+++ src/com/luan/cursojava/aula20/Matriz.java
@@ -5,18 +5,17 @@
  */
 package com.luan.cursojava.aula20;
 
-/**
- *
- * @author Luan
- */
+// tirando a média da nota de alunos, usando matrizes
 public class Matriz {
     public static void main(String[] args) {
         
         
-        double[][] notasAlunos = new double[30][4];
+        // declarando uma matriz diretamente.
+        double notas[][] = {{5,6,9,8}, {6,6,9,9}};
+        double[][] notasAlunos = new double[3][4];
         
-        notasAlunos[0][0] = 10;
-        notasAlunos[0][1] = 7;
+        notasAlunos[0][0] = 2;
+        notasAlunos[0][1] = 5;
         notasAlunos[0][2] = 9;
         notasAlunos[0][3] = 9.5;
         
@@ -26,11 +25,22 @@ public class Matriz {
         notasAlunos[1][3] = 9.5;
         
         
-        notasAlunos[2][0] = 10;
+        notasAlunos[2][0] = 6;
         notasAlunos[2][1] = 7;
         notasAlunos[2][2] = 9;
-        notasAlunos[2][3] = 9.5;
+        notasAlunos[2][3] = 8;
         
+        double soma;
+        for (int i = 0; i <notasAlunos.length; i++) {
+            soma = 0;
+            for (int j = 0; j <notasAlunos[i].length; j++) {
+                soma += notasAlunos[i][j];
+            }
+            
+            System.out.println("Média do aluno: "+i + " é = "+ (soma/4));
+        }
+        
+       
         
         
     }
