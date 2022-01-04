d587f65b17c33637ff99ef8f7d65be92d131712a
luan3642 <luan3642@hotmail.com>
2022-01-04 12:11:53 -0300

    ...

--- src/exercicios/aula20/Ex5.java
+++ src/exercicios/aula20/Ex5.java
@@ -12,84 +12,116 @@ import java.util.Scanner;
  * @author Luan
  */
 public class Ex5 {
-
-    public static String compromisso[][][] = new String[13][32][25];
-
-    public void agendarCompromisso(int mes, int dia, int hora, String tarefa) {
-        for (int i = 0; i < compromisso.length; i++) {
-            for (int j = 0; j < compromisso[i].length; j++) {
-                for (int k = 0; k < compromisso[i][j].length; k++) {
-                    if (k <= 8) {
-                        compromisso[mes][dia][hora] = tarefa;
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-
-    }
-
-    public String pesquisarCompromisso(int mes, int dia, int hora) {
-        int auxMes = 0, diaAux = 0, auxHora = 0;
-
-        for (int i = 0; i < compromisso.length; i++) {
-            for (int j = 0; j < compromisso[i].length; j++) {
-                for (int k = 0; k < compromisso[i][j].length; k++) {
-                    if (k == hora) {
-                        auxHora = k;
-                    }
-                }
-            }
-        }
-        return ("Tarefa: " + compromisso[mes][dia][hora] + " Horário: " + auxHora);
-
-    }
-
     public static void main(String[] args) {
-        Ex5 ex5 = new Ex5();
-
+        
         Scanner scan = new Scanner(System.in);
-
-        boolean pergunta = true;
-
-        while (pergunta != false) {
-
-            System.out.println("Informe o mês da tarefa");
-            int mes = scan.nextInt();
-
-            System.out.println("Informe o dia do compromisso");
-            int dia = scan.nextInt();
-
-            System.out.println("Informe o horário do compromisso ");
-            int horario = scan.nextInt();
-            System.out.println("Informe a tarefa");
-            String tarefa = scan.next();
-
-            ex5.agendarCompromisso(mes, dia, horario, tarefa);
-
-            System.out.println("");
-            System.out.println("Deseja cadastrar uma tarefa nova?");
-            pergunta = scan.nextBoolean();
-
-            System.out.println("");
-
+        
+        String[][][] compromissos = new String[12][31][8];
+        
+        boolean sair = false;
+        byte opcao;
+        while (!sair){
+            
+            System.out.println("Digite 1 para adicionar compromisso.");
+            System.out.println("Digite 2 para verificar compromisso.");
+            System.out.println("Digite 0 para sair.");
+            
+            opcao = scan.nextByte();
+            
+            if (opcao == 1){ //adicionar compromisso
+                
+                
+                boolean mesValido = false;
+                int mes = 0;
+                while (!mesValido){
+                    System.out.println("Entre com o mês");
+                    mes = scan.nextInt();
+                    if (mes > 0 && mes <= 12){
+                        mesValido = true;
+                    } else {
+                        System.out.println("Mês inválido, digite novamente");
+                    }
+                }
+                
+                
+                boolean diaValido = false;
+                int dia = 0;
+                while (!diaValido){
+                    System.out.println("Entre com o dia do mês");
+                    dia = scan.nextInt();
+                    if (dia > 0 && dia <= 31){
+                        diaValido = true;
+                    } else {
+                        System.out.println("Dia inválido, digite novamente");
+                    }
+                }
+                
+                boolean horaValida = false;
+                int hora = 0;
+                while (!horaValida){
+                    System.out.println("Entre com a hora do compromisso");
+                    hora = scan.nextInt();
+                    if (hora >= 0 && hora <= 8){
+                        horaValida = true;
+                    } else {
+                        System.out.println("Hora inválida, digite novamente");
+                    }
+                }
+                mes--;
+                dia--;
+                System.out.println("Digite o compromisso");
+                compromissos[mes][dia][hora] = scan.next();
+                
+            } else if (opcao == 2){ //verificar compromisso
+                
+                boolean mesValido = false;
+                int mes = 0;
+                while (!mesValido){
+                    System.out.println("Entre com o mês");
+                    mes = scan.nextInt();
+                    if (mes > 0 && mes <= 12){
+                        mesValido = true;
+                    } else {
+                        System.out.println("Mês inválido, digite novamente");
+                    }
+                }
+                
+                
+                boolean diaValido = false;
+                int dia = 0;
+                while (!diaValido){
+                    System.out.println("Entre com o dia do mês");
+                    dia = scan.nextInt();
+                    if (dia > 0 && dia <= 31){
+                        diaValido = true;
+                    } else {
+                        System.out.println("Dia inválido, digite novamente");
+                    }
+                }
+                
+                boolean horaValida = false;
+                int hora = 0;
+                while (!horaValida){
+                    System.out.println("Entre coma hora do compromisso");
+                    hora = scan.nextInt();
+                    if (hora >= 0 && hora <= 24){
+                        horaValida = true;
+                    } else {
+                        System.out.println("Hora inválida, digite novamente");
+                    }
+                }
+                mes--;
+                dia--;
+                System.out.println("O compromisso agendado é:");
+                System.out.println(compromissos[mes] [dia][hora]);
+                
+            } else if (opcao == 0){
+                sair = true;
+            } else {
+                System.out.println("Opção inválida, digite novamente");
+            }
         }
-        System.out.println("Deseja pesquisar um compromisso?");
-        boolean confirmacao = scan.nextBoolean();
-
-        if (confirmacao == true) {
-
-            System.out.println("informe o mês");
-            int mes = scan.nextInt();
-
-            System.out.println("Informe o dia ");
-            int dia = scan.nextInt();
-
-            System.out.println("Informe o horário");
-            int horario = scan.nextInt();
-
-            System.out.println(ex5.pesquisarCompromisso(mes, dia, horario));
-        }
+        
     }
+    
 }
