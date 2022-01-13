0ca57a04e22fd81d047f592dcb415b8e878a6200
luan3642 <luan3642@hotmail.com>
2022-01-13 19:05:53 -0300

    ...

---
+++ src/com/luan/cursojava/aula28/Pacotes.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luan.cursojava.aula28;
+
+import com.luan.cursojava.aula27.Carro;
+import exercicios.aula19.BubbleSort;
+/**
+ *
+ * @author Luan
+ */
+public class Pacotes {
+    public static void main(String[] args) {
+        
+        
+        Carro carro;
+        com.luan.cursojava.aula24.Carro carro2;
+        
+        
+        exercicios.aula13.Ex1 ex1;
+        BubbleSort b = new BubbleSort();
+        
+        
+    }
+}
