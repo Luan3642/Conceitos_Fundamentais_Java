41a5a9004059709647914d27ef32655ed6b5048f
luan3642 <luan3642@hotmail.com>
2022-01-15 18:58:30 -0300

    ...

---
+++ src/com/luan/cursojava/aula32/TesteCarro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luan.cursojava.aula32;
+
+/**
+ *
+ * @author Luan
+ */
+public class TesteCarro {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        
+        
+        carro.setMarca("Ducato");
+        
+        System.out.println(carro.getMarca());
+    }
+}
