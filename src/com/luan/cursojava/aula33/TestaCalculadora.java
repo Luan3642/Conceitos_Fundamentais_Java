f2002f6045537423b8fa0e6be734b965b4e4ea68
luan3642 <luan3642@hotmail.com>
2022-01-17 21:38:39 -0300

    ...

---
+++ src/com/luan/cursojava/aula33/TestaCalculadora.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luan.cursojava.aula33;
+
+/**
+ *
+ * @author Luan
+ */
+public class TestaCalculadora {
+    public static void main(String[] args) {
+        MinhaCalculadora minhaCalculadora = new MinhaCalculadora();
+        
+        minhaCalculadora.soma(1, 2);
+        minhaCalculadora.soma(2.0, 3.0)
+    }
+   
+}
