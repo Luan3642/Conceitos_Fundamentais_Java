579eabdae1e479ec0d12bdcc1810b5a229418767
luan3642 <luan3642@hotmail.com>
2022-01-19 23:27:01 -0300

    ...

---
+++ src/exercicios/aula28/JogoVelha.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios.aula28;
+
+/**
+ *
+ * @author Luan
+ */
+public class JogoVelha {
+    private boolean ganhador;
+    private int jogador;
+    private char sinal;
+    private char tabuleiro[][];
+    
+    
+    public JogoVelha(){
+        tabuleiro = new char[3][3];
+    }
+    
+    
+}
