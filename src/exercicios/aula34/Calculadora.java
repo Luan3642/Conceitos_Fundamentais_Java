28279a9db72c2bd3a821f79db44f3537f20134cd
luan3642 <luan3642@hotmail.com>
2022-01-21 12:49:31 -0300

    ...

---
+++ src/exercicios/aula34/Calculadora.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios.aula34;
+
+/**
+ *
+ * @author Luan
+ */
+public class Calculadora {
+
+    public static int somar(int n1, int n2) {
+        return n1 + n2;
+    }
+    
+    public static int subtrair(int n1, int n2){
+        return n1 - n2;
+    }
+    
+    public static int multiplicar(int n1, int n2){
+        return n1 * n2;
+    }
+    
+    public static int dividir(int n1, int n2){
+        return n1 / n2;
+    }
+    
+    public static double potencia(double n1){
+        return Math.pow(n1, 2);
+    }
+    
+    
+}
---
+++ src/exercicios/aula34/TestaCalculadora.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios.aula34;
+
+/**
+ *
+ * @author Luan
+ */
+public class TestaCalculadora {
+    public static void main(String[] args) {
+        System.out.println(Calculadora.somar(2, 3));
+        System.out.println(Calculadora.potencia(2));
+    }
+}
