3edf7523cd4213c7811ca53cff0deaf145f40205
luan3642 <luan3642@hotmail.com>
2022-02-01 21:27:31 -0300

    ...

---
+++ src/exercicios/aula37/Teste.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicios.aula37;
+
+/**
+ *
+ * @author Luan
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+
+//        Aluno aluno = new Aluno();
+//        
+//        Professor professor = new Professor();
+//        
+//        aluno.setNome("Maria");
+//        
+//        aluno.getTelefoneCelular();
+
+    Pessoa aluno = new Aluno();
+    
+    Pessoa professor = new Professor();
+    
+    
+    }
+}
