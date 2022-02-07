008d4c00d68f22c476acbe638297e710897ec7bc
luan3642 <luan3642@hotmail.com>
2022-02-06 23:31:15 -0300

    ....

---
+++ src/com/luan/cursojava/aula42/Constantes.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luan.cursojava.aula42;
+
+/**
+ *
+ * @author Luan
+ */
+public final class Constantes {
+    public static final String URL_BLOG = "http://loiane.com";
+    public static final String CURSO_COMPLETO = "http://loiane.com.training";
+    
+    
+}
---
+++ src/com/luan/cursojava/aula42/Teste.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.luan.cursojava.aula42;
+
+/**
+ *
+ * @author Luan
+ */
+public class Teste {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        Aluno aluno = new Aluno();
+        Professor professor = new Professor();
+       
+        
+        final int TOTAL = 1;
+//        TOTAL +=1;
+        
+        
+    }
+}
