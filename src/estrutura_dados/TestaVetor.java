177016d3995d80a0ba8aede8d5deac1933a239a5
luan3642 <luan3642@hotmail.com>
2022-03-04 18:27:49 -0300

    ....

---
+++ src/estrutura_dados/TestaVetor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estrutura_dados;
+
+/**
+ *
+ * @author Luan
+ */
+public class TestaVetor {
+    public static void main(String[] args) {
+        
+        
+        Vetor vetor = new Vetor(3);
+        
+        int i = 0;
+        i++;
+    }
+}
---
+++ src/estrutura_dados/Vetor.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estrutura_dados;
+
+/**
+ *
+ * @author Luan
+ */
+public class Vetor {
+    private String[] elementos;
+    
+    
+   
+    public Vetor(int capacidade){
+        this.elementos = new String[capacidade];
+    }
+}
