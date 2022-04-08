720e8b13400736b7fd5fc5e5e152355cfafaf5b2
luan3642 <luan3642@hotmail.com>
2022-04-08 00:08:36 -0300

    ...

--- src/estrutura_dados/TestaVetor.java
+++ src/estrutura_dados/TestaVetor.java
@@ -5,16 +5,26 @@
  */
 package estrutura_dados;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Luan
  */
 public class TestaVetor {
+
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(5);
-        
-        int  i = 0;
-        i++;
+        Vetor vetor = new Vetor(2);
+
+        try {
+            vetor.adiciona("elemento 1");
+            vetor.adiciona("elemento 2");
+            vetor.adiciona("elemento 3");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
     }
- 
+
 }
--- src/estrutura_dados/Vetor.java
+++ src/estrutura_dados/Vetor.java
@@ -10,11 +10,32 @@ package estrutura_dados;
  * @author Luan
  */
 public class Vetor {
-    
-   private String[] elementos;
-   
-   
-   public Vetor(int capacidade){
-       this.elementos = new String[capacidade];
-   }
+
+    private String[] elementos;
+    private int tamanho;
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+        this.tamanho = 0;
+    }
+
+    // método ineficiente em termos de processameto
+//   public void adiciona(String elemento){
+//       for (int i = 0; i < this.elementos.length; i++) {
+//           if(this.elementos[i] == null){
+//               this.elementos[i] = elemento;
+//               break;
+//           }
+//       }
+//   }
+    public void adiciona(String elemento) throws Exception  {
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+        } else{
+            throw new Exception("Vetor já está cheio");
+        }
+
+    }
+
 }
