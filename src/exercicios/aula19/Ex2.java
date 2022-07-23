7e9c6900002d51293e97bbb1cd4b80593787d6d1
luan3642 <luan3642@hotmail.com>
2022-07-23 19:52:31 -0300

    ...'

--- src/exercicios/aula19/Ex2.java
+++ src/exercicios/aula19/Ex2.java
@@ -6,46 +6,35 @@
 package exercicios.aula19;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Ex2 {
-
-    public static void vetores() {
-
-        Random rd = new Random();
-        //
-        
-        ///
-        //
+    public static void main(String[] args) {
         int[] vetorA = new int[8];
         int[] vetorB = new int[vetorA.length];
-
-        for (int i = 0; i < vetorA.length; i++) {
-            vetorA[i] = rd.nextInt(10);
+        
+        
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i <vetorA.length; i++) {
+            System.out.println("Informe o valor de A");
+            vetorA[i] = scan.nextInt();
+            vetorB[i] = vetorA[i] *2;
         }
-
+        
         System.out.println("");
-
-        System.out.println("Valores vetorA");
-
-        for (int y : vetorA) {
-            System.out.println(y);
+        System.out.println("Valor de A");
+        for(int c : vetorA){
+            System.out.println(c);
         }
-
-        for (int v = 0; v < vetorB.length; v++) {
-            vetorB[v] = vetorA[v] * 2;
-        }
-
+        
         System.out.println("");
-        System.out.println("Valores vetorB");
-
-        for (int b : vetorB) {
-            System.out.println(b);
+        System.out.println("Valor de B");
+        
+        for (int i : vetorB) {
+            System.out.println(i);
         }
-//////////
+        
     }
-
-    public static void main(String[] args) {
-        vetores();
-    }
+    
 }
 ///
